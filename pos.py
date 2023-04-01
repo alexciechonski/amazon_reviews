21df031efa766abe9ae350702b4cccad92800b06
alexciechonski <101641507+alexciechonski@users.noreply.github.com>
2023-04-01 17:54:17 +0100

    Add files via upload

--- pos.py
+++ pos.py
@@ -14,3 +14,44 @@ txt_correct = nlp('Chesapeake Bay Candle PT41312 Scented Candle, Refresh + Rejuv
 for token in txt_correct:
    print(token,'|',token.tag_, '|', spacy.explain(token.tag_))
 
+
+'''
+OUTPUT:
+Glade | NNP | noun, proper singular
+Candle | NNP | noun, proper singular <---
+Jar | NNP | noun, proper singular
+, | , | punctuation mark, comma
+Air | NNP | noun, proper singular
+Freshener | NNP | noun, proper singular
+, | , | punctuation mark, comma
+2in1 | CD | cardinal number
+, | , | punctuation mark, comma
+Moonlit | NNP | noun, proper singular
+Walk | NNP | noun, proper singular
+& | CC | conjunction, coordinating
+Wandering | NNP | noun, proper singular
+Stream | NNP | noun, proper singular
+, | , | punctuation mark, comma
+3.4 | CD | cardinal number
+Oz | NN | noun, singular or mass
+, | , | punctuation mark, comma
+2 | CD | cardinal number
+Count | NNP | noun, proper singular
+---------------------------------------------------------------------
+Chesapeake | NNP | noun, proper singular
+Bay | NNP | noun, proper singular
+Candle | NNP | noun, proper singular  <---
+PT41312 | NN | noun, singular or mass
+Scented | NNP | noun, proper singular
+Candle | NNP | noun, proper singular
+, | , | punctuation mark, comma
+Refresh | NNP | noun, proper singular
++ | NNP | noun, proper singular
+Rejuvenate | NNP | noun, proper singular
+( | -LRB- | left round bracket
+Mediterranean | NNP | noun, proper singular
+Citrus | NNP | noun, proper singular
+) | -RRB- | right round bracket
+, | , | punctuation mark, comma
+Medium | NN | noun, singular or mass
+'''
